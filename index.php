5e01d62f1601904bbea3fdfa214c75b6f29fe79c
CapitanPulgarin <kaputbutton@gmail.com>
2023-02-28 15:04:03 -0500

    html

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <title>Mi Primera Página</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor_izq"><img class="img1" src="imagenes/halojefe.jpg" alt="img izquierda"></div>
+        <div class="contenedor_cent"><strong> HALO </strong></div>
+        <div class="contenedor_der"><img class="img1" src="imagenes/segund.jpg" alt="img derecha"></div>
+
+    </header>
+    <nav></nav>
+    <section></section>
+    <aside></aside>
+    <footer></footer>
+
+</body>
+</html>
